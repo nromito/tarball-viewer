aa1f60b13152bd77afa22b3e5d5009e25f63d27a
Nick Romito <nick@cribl.io>
2021-11-27 14:39:18 -0600

    Create custom editor to handle .tgz files

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { parse } from 'path';
 import * as vscode from 'vscode';
 import { Tarball } from './tar';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tarball-viewer" is now active!');
-	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider('tarball', new TarballContentProvider));
-	const disposable = vscode.workspace.onDidOpenTextDocument(async doc => {
-		console.log('doc opened', {doc: doc.fileName});
-		const ext = parse(doc.fileName).ext;
-		if (ext !== '.tgz') {return;}
-		const uri = vscode.Uri.parse('tarball:' + doc.fileName);
-		const newdoc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
-		await vscode.window.showTextDocument(newdoc, { preview: false });
-	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.window.registerCustomEditorProvider('tarball-viewer.tarballPreview', new TarballEditorProvider));
 }
 
-class TarballContentProvider implements vscode.TextDocumentContentProvider {
-	private emitter = new vscode.EventEmitter<vscode.Uri>();
-	onDidChange?: vscode.Event<vscode.Uri> | undefined;
-	constructor() {
-		this.onDidChange = this.emitter.event;
+class TarballEditorProvider implements vscode.CustomReadonlyEditorProvider<TarballListingDocument> {
+	openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken): TarballListingDocument | Thenable<TarballListingDocument> {
+		return new TarballListingDocument(uri);
 	}
-	provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
-		console.log('providing text doc content', {uri: uri.fsPath});
-		return new Promise(async (resolve, reject) => {
-			try {
-				const tarball = new Tarball(uri.fsPath);
-				const entries = await tarball.listContent();
-				console.log('got entries', {entries});
-				return resolve(entries.join('\n'));
-			} catch (err) {
-				console.log('failed to get content', {err});
-				reject(err);
-			}
+	resolveCustomEditor(document: TarballListingDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
+		return new Promise(async resolve => {
+			webviewPanel.webview.html = `
+			<!DOCTYPE html>
+			<html lang="en">
+			<body>
+				<div>
+					${(await document.getContent()).replace(/(?:\r\n|\n|\r)/g, '<br>')}
+				</div>
+			</body>
+			</html>
+			`;
+			resolve();
 		});
+	}
+}
+
+class TarballListingDocument implements vscode.CustomDocument {
+	private tarball: Tarball;
+	constructor(readonly uri: vscode.Uri) {
+		this.tarball = new Tarball(uri.fsPath);
+	}
+	dispose(): void {
+		// TODO
+	}
+	public async getContent(): Promise<string> {
+		const listing = await this.tarball.listContent();
+		return listing.join('\n');
 	}
 }
 
