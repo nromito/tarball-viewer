ae1e2fa7447c68d873761ca1cd9bb565466054cf
Nick Romito <nick@cribl.io>
2021-11-27 19:06:29 -0600

    updating tabs to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -4,90 +4,90 @@ import * as vscode from 'vscode';
 import { Tarball } from './tar';
 
 interface IReadTarballFileOpts {
-	tarballUri: string;
-	fileName: string;
+  tarballUri: string;
+  fileName: string;
 }
 const COMMAND_READ_TARBALL_FILE = 'tarball-viewer.readTarballFile';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand(COMMAND_READ_TARBALL_FILE, onReadTarballFile));
-	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider('tarball', new TarballContentProvider()));
-	context.subscriptions.push(vscode.window.registerCustomEditorProvider('tarball-viewer.tarballPreview', new TarballEditorProvider()));
+  context.subscriptions.push(vscode.commands.registerCommand(COMMAND_READ_TARBALL_FILE, onReadTarballFile));
+  context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider('tarball', new TarballContentProvider()));
+  context.subscriptions.push(vscode.window.registerCustomEditorProvider('tarball-viewer.tarballPreview', new TarballEditorProvider()));
 }
 
 class TarballEditorProvider implements vscode.CustomReadonlyEditorProvider<TarballDocument> {
-	openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken): TarballDocument | Thenable<TarballDocument> {
-		return new TarballDocument(uri);
-	}
-	resolveCustomEditor(document: TarballDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
-		webviewPanel.webview.options = {
-			enableCommandUris: true,
-		};
-		return new Promise(async resolve => {
-			const links = (await document.getListing())
-				.map(file => `<a href=${document.getUriFor(file)}>${file}</a>`);
-			webviewPanel.webview.html = `
-			<!DOCTYPE html>
-			<html lang="en">
-			<body>
-				<div>
-					${links.join('<br>')}
-				</div>
-			</body>
-			</html>
-			`;
-			resolve();
-		});
-	}
+  openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken): TarballDocument | Thenable<TarballDocument> {
+    return new TarballDocument(uri);
+  }
+  resolveCustomEditor(document: TarballDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
+    webviewPanel.webview.options = {
+      enableCommandUris: true,
+    };
+    return new Promise(async resolve => {
+      const links = (await document.getListing())
+        .map(file => `<a href=${document.getUriFor(file)}>${file}</a>`);
+      webviewPanel.webview.html = `
+      <!DOCTYPE html>
+      <html lang="en">
+      <body>
+        <div>
+          ${links.join('<br>')}
+        </div>
+      </body>
+      </html>
+      `;
+      resolve();
+    });
+  }
 }
 
 class TarballDocument implements vscode.CustomDocument {
-	private tarball: Tarball;
-	constructor(readonly uri: vscode.Uri) {
-		this.tarball = new Tarball(uri.fsPath);
-	}
-	dispose(): void {
-		// TODO
-	}
-	public async getListing(): Promise<string[]> {
-		return this.tarball.listContent();
-	}
-	public getUriFor(listing: string): vscode.Uri {
-		const opts: IReadTarballFileOpts = {
-			tarballUri: this.uri.toString(),
-			fileName: listing,
-		};
-		return vscode.Uri.parse(`command:${COMMAND_READ_TARBALL_FILE}?${encodeURIComponent(JSON.stringify([opts]))}`);
-	}
+  private tarball: Tarball;
+  constructor(readonly uri: vscode.Uri) {
+    this.tarball = new Tarball(uri.fsPath);
+  }
+  dispose(): void {
+    // TODO
+  }
+  public async getListing(): Promise<string[]> {
+    return this.tarball.listContent();
+  }
+  public getUriFor(listing: string): vscode.Uri {
+    const opts: IReadTarballFileOpts = {
+      tarballUri: this.uri.toString(),
+      fileName: listing,
+    };
+    return vscode.Uri.parse(`command:${COMMAND_READ_TARBALL_FILE}?${encodeURIComponent(JSON.stringify([opts]))}`);
+  }
 }
 
 class TarballContentProvider implements vscode.TextDocumentContentProvider {
-	private emitter = new vscode.EventEmitter<vscode.Uri>();
-	onDidChange?: vscode.Event<vscode.Uri> | undefined;
-	constructor() {
-		this.onDidChange = this.emitter.event;
-	}
-	provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
-		return new Promise(async (resolve, reject) => {
-			try {
-				const uriString = uri.toString(true);
-				const parts = uriString.split(':');
-				console.log('got tarball uri', {uri: uriString, parts});
-				const [tarballPath, filePath] = parts.slice(-2);
-				const tarball = new Tarball(tarballPath);
-				return resolve(tarball.getFileContent(filePath));
-			} catch (err) {
-				reject(err);
-			}
-		});
-	}
+  private emitter = new vscode.EventEmitter<vscode.Uri>();
+  onDidChange?: vscode.Event<vscode.Uri> | undefined;
+  constructor() {
+    this.onDidChange = this.emitter.event;
+  }
+  provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const uriString = uri.toString(true);
+        const parts = uriString.split(':');
+        console.log('got tarball uri', {uri: uriString, parts});
+        const [tarballPath, filePath] = parts.slice(-2);
+        const tarball = new Tarball(tarballPath);
+        return resolve(tarball.getFileContent(filePath));
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 }
 
 async function onReadTarballFile(opts: IReadTarballFileOpts): Promise<void> {
-	const uri = vscode.Uri.parse(`tarball:${opts.tarballUri}:${opts.fileName}`);
-	const newdoc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
-	await vscode.window.showTextDocument(newdoc, { preview: false });
+  const uri = vscode.Uri.parse(`tarball:${opts.tarballUri}:${opts.fileName}`);
+  const newdoc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
+  await vscode.window.showTextDocument(newdoc, { preview: false });
 }
 
 // this method is called when your extension is deactivated
--- src/tar.ts
+++ src/tar.ts
@@ -8,64 +8,64 @@ export class Tarball {
   public async listContent(): Promise<string[]> {
     console.log('listing tarball content', {tarball: this.tarball});
     return await new Promise(async (resolve, reject) => {
-			try {
-				const extract = tar.extract();
-				const contents: string[] = [];
-				extract.on('entry', (header: any, stream: any, next: any) => {
-					if (header.type !== 'directory') {
-						contents.push(header.name);
-					}
-					stream.on('end', function() {
-						next(); // ready for next entry
-					});
-					stream.resume();
-				});
-				const streams = [
-					createReadStream(this.tarball),
-					this.tarball.endsWith('gz') ? createGunzip() : undefined,
-					extract
-				].filter(s => !!s);
-				pipeline(streams, err => {
-					console.log('finished pipeline', {err, contents});
-					if (err) {return reject(err);}
-					return resolve(contents);
-				});
-			} catch (err) {
-				reject(err);
-			}
-		});
+      try {
+        const extract = tar.extract();
+        const contents: string[] = [];
+        extract.on('entry', (header: any, stream: any, next: any) => {
+          if (header.type !== 'directory') {
+            contents.push(header.name);
+          }
+          stream.on('end', function() {
+            next(); // ready for next entry
+          });
+          stream.resume();
+        });
+        const streams = [
+          createReadStream(this.tarball),
+          this.tarball.endsWith('gz') ? createGunzip() : undefined,
+          extract
+        ].filter(s => !!s);
+        pipeline(streams, err => {
+          console.log('finished pipeline', {err, contents});
+          if (err) {return reject(err);}
+          return resolve(contents);
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
   }
   public async getFileContent(file: string): Promise<string> {
     console.log('getting tarball file content', {tarball: this.tarball, file});
     return await new Promise(async (resolve, reject) => {
-			try {
-				const extract = tar.extract();
-				let content = '';
-				extract.on('entry', (header: any, stream: any, next: any) => {
-					if (header.name === file) {
+      try {
+        const extract = tar.extract();
+        let content = '';
+        extract.on('entry', (header: any, stream: any, next: any) => {
+          if (header.name === file) {
             stream.on('data', buf => {
               content += buf.toString();
             });
             // TODO: kill extract stream on stream end to stop reading the tarball
           }
-					stream.on('end', function() {
-						next(); // ready for next entry
-					});
-					stream.resume();
-				});
-				const streams = [
-					createReadStream(this.tarball),
-					this.tarball.endsWith('gz') ? createGunzip() : undefined,
-					extract
-				].filter(s => !!s);
-				pipeline(streams, err => {
-					console.log('finished pipeline', {err});
-					if (err) {return reject(err);}
-					return resolve(content);
-				});
-			} catch (err) {
-				reject(err);
-			}
+          stream.on('end', function() {
+            next(); // ready for next entry
+          });
+          stream.resume();
+        });
+        const streams = [
+          createReadStream(this.tarball),
+          this.tarball.endsWith('gz') ? createGunzip() : undefined,
+          extract
+        ].filter(s => !!s);
+        pipeline(streams, err => {
+          console.log('finished pipeline', {err});
+          if (err) {return reject(err);}
+          return resolve(content);
+        });
+      } catch (err) {
+        reject(err);
+      }
     });
   }
 }
