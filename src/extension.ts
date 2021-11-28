c09222d800b2f8f5d5f965a16dafe7498c6f41e3
Nick Romito <nick@cribl.io>
2021-11-27 18:35:23 -0600

    Get linking to tarball files working

--- src/extension.ts
+++ src/extension.ts
@@ -3,24 +3,36 @@
 import * as vscode from 'vscode';
 import { Tarball } from './tar';
 
+interface IReadTarballFileOpts {
+	tarballUri: string;
+	fileName: string;
+}
+const COMMAND_READ_TARBALL_FILE = 'tarball-viewer.readTarballFile';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.window.registerCustomEditorProvider('tarball-viewer.tarballPreview', new TarballEditorProvider));
+	context.subscriptions.push(vscode.commands.registerCommand(COMMAND_READ_TARBALL_FILE, onReadTarballFile));
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider('tarball', new TarballContentProvider()));
+	context.subscriptions.push(vscode.window.registerCustomEditorProvider('tarball-viewer.tarballPreview', new TarballEditorProvider()));
 }
 
-class TarballEditorProvider implements vscode.CustomReadonlyEditorProvider<TarballListingDocument> {
-	openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken): TarballListingDocument | Thenable<TarballListingDocument> {
-		return new TarballListingDocument(uri);
+class TarballEditorProvider implements vscode.CustomReadonlyEditorProvider<TarballDocument> {
+	openCustomDocument(uri: vscode.Uri, openContext: vscode.CustomDocumentOpenContext, token: vscode.CancellationToken): TarballDocument | Thenable<TarballDocument> {
+		return new TarballDocument(uri);
 	}
-	resolveCustomEditor(document: TarballListingDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
+	resolveCustomEditor(document: TarballDocument, webviewPanel: vscode.WebviewPanel, token: vscode.CancellationToken): void | Thenable<void> {
+		webviewPanel.webview.options = {
+			enableCommandUris: true,
+		};
 		return new Promise(async resolve => {
+			const links = (await document.getListing())
+				.map(file => `<a href=${document.getUriFor(file)}>${file}</a>`);
 			webviewPanel.webview.html = `
 			<!DOCTYPE html>
 			<html lang="en">
 			<body>
 				<div>
-					${(await document.getContent()).replace(/(?:\r\n|\n|\r)/g, '<br>')}
+					${links.join('<br>')}
 				</div>
 			</body>
 			</html>
@@ -30,7 +42,7 @@ class TarballEditorProvider implements vscode.CustomReadonlyEditorProvider<Tarba
 	}
 }
 
-class TarballListingDocument implements vscode.CustomDocument {
+class TarballDocument implements vscode.CustomDocument {
 	private tarball: Tarball;
 	constructor(readonly uri: vscode.Uri) {
 		this.tarball = new Tarball(uri.fsPath);
@@ -38,10 +50,44 @@ class TarballListingDocument implements vscode.CustomDocument {
 	dispose(): void {
 		// TODO
 	}
-	public async getContent(): Promise<string> {
-		const listing = await this.tarball.listContent();
-		return listing.join('\n');
+	public async getListing(): Promise<string[]> {
+		return this.tarball.listContent();
 	}
+	public getUriFor(listing: string): vscode.Uri {
+		const opts: IReadTarballFileOpts = {
+			tarballUri: this.uri.toString(),
+			fileName: listing,
+		};
+		return vscode.Uri.parse(`command:${COMMAND_READ_TARBALL_FILE}?${encodeURIComponent(JSON.stringify([opts]))}`);
+	}
+}
+
+class TarballContentProvider implements vscode.TextDocumentContentProvider {
+	private emitter = new vscode.EventEmitter<vscode.Uri>();
+	onDidChange?: vscode.Event<vscode.Uri> | undefined;
+	constructor() {
+		this.onDidChange = this.emitter.event;
+	}
+	provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): vscode.ProviderResult<string> {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const uriString = uri.toString(true);
+				const parts = uriString.split(':');
+				console.log('got tarball uri', {uri: uriString, parts});
+				const [tarballPath, filePath] = parts.slice(-2);
+				const tarball = new Tarball(tarballPath);
+				return resolve(tarball.getFileContent(filePath));
+			} catch (err) {
+				reject(err);
+			}
+		});
+	}
+}
+
+async function onReadTarballFile(opts: IReadTarballFileOpts): Promise<void> {
+	const uri = vscode.Uri.parse(`tarball:${opts.tarballUri}:${opts.fileName}`);
+	const newdoc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
+	await vscode.window.showTextDocument(newdoc, { preview: false });
 }
 
 // this method is called when your extension is deactivated
--- src/tar.ts
+++ src/tar.ts
@@ -6,6 +6,7 @@ import * as tar from 'tar-stream';
 export class Tarball {
   constructor(private tarball: string) {}
   public async listContent(): Promise<string[]> {
+    console.log('listing tarball content', {tarball: this.tarball});
     return await new Promise(async (resolve, reject) => {
 			try {
 				const extract = tar.extract();
@@ -33,5 +34,38 @@ export class Tarball {
 				reject(err);
 			}
 		});
+  }
+  public async getFileContent(file: string): Promise<string> {
+    console.log('getting tarball file content', {tarball: this.tarball, file});
+    return await new Promise(async (resolve, reject) => {
+			try {
+				const extract = tar.extract();
+				let content = '';
+				extract.on('entry', (header: any, stream: any, next: any) => {
+					if (header.name === file) {
+            stream.on('data', buf => {
+              content += buf.toString();
+            });
+            // TODO: kill extract stream on stream end to stop reading the tarball
+          }
+					stream.on('end', function() {
+						next(); // ready for next entry
+					});
+					stream.resume();
+				});
+				const streams = [
+					createReadStream(this.tarball),
+					this.tarball.endsWith('gz') ? createGunzip() : undefined,
+					extract
+				].filter(s => !!s);
+				pipeline(streams, err => {
+					console.log('finished pipeline', {err});
+					if (err) {return reject(err);}
+					return resolve(content);
+				});
+			} catch (err) {
+				reject(err);
+			}
+    });
   }
 }
